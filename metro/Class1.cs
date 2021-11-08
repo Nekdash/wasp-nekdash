cbb28a52abf12374aa30789c7a1454efb3c4b52d
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-11-08 17:21:42 +0300

    metro1

---
+++ metro/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Metro
+{
+    public List<Line> lines;
+    public string city;
+    public Metro(string city_in)
+    {
+        lines = new List<Line>();
+        city = city_in;
+    }
+    public string GetCity()
+    {
+        return city;
+    }
+    public void AddLine(string name, string color)
+    {
+        lines.Add(new Line(name, color));
+
+    }
+    public void RemoveLine(string name)
+    {
+        for (int i = 0; i < lines.Count; i++)
+        {
+            if (lines[i].name == name)
+            {
+                lines.RemoveAt(i);
+            }
+        }
+    }
+
+}
---
+++ metro/Class3.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+public class Station
+{
+    public string name;
+    public string color;
+    public Line line;
+    public bool isWheelchairAccessible;
+    public bool hasParkandRide;
+    public bool hasNearbyCableCar;
+    public List<Station> transfers;
+
+    public Station(string name, string color)
+    {
+        this.name = name;
+        this.color = color;
+    }
+    public Station(string name, string color, List<Station> transfers)
+    {
+        this.name = name;
+        this.color = color;
+        this.transfers = transfers;
+    }
+    public string GetName()
+    {
+        return this.name;
+    }
+    public void SetName(string name)
+    {
+        this.name = name;
+    }
+    public bool IsWheelchairAccessible()
+    {
+        try
+        {
+            return isWheelchairAccessible;
+        }
+        catch
+        {
+            return false;
+        }
+
+    }
+    public bool HasParkandRide()
+    {
+        try
+        {
+            return hasParkandRide;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    public bool HasNearbyCableCar()
+    {
+        try
+        {
+            return hasNearbyCableCar;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    public string GetLineName()
+    {
+        return line.name;
+    }
+    public List<Station> GetTransferList()
+    {
+        return transfers;
+    }
+
+}
